25385571264b8f4a2f00373a68c1b6715067e12c
Varmais1 <418487@bsd48.org>
2022-09-23 13:37:10 -0700

    Blank board printing

---
+++ TicTacToe.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+const int blank = 0;
+const int xSpace = 1;
+const int oSpace = 2;
+
+
+void displayBoard(int board[3][3]);
+
+int main() {
+  int board[3][3] = { {0,0,0}, {0,0,0}, {0,0,0} };
+  displayBoard(board);
+  
+  
+  
+  return 0;
+}
+
+void displayBoard(int board[3][3]) {
+  cout << "\t1\t2\t3" << endl;
+  for(int i = 0; i < 3; i++) {
+    int character = 97 + i;
+    char boardValues[3];
+    for(int j = 0; j < 3; j++) {
+      if(board[i][j] == blank) {
+	boardValues[j] = ' ';
+      }
+      else if(board[i][j] == xSpace) {
+	boardValues[j] = 'X';
+      }
+      else if(board[i][j] == oSpace) {
+	boardValues[j] = 'O';
+      }
+    }
+    cout << (char)character << "\t" << boardValues[0] << "\t" << boardValues[1] << "\t" << boardValues[2] << endl;
+  }
+}
